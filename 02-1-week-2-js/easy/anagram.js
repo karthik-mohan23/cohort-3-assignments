a55d9fe296c46267e64b72346577389998be8091
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-08-31 11:51:29 +0530

    completed week 2 easy ones

---
+++ 02-1-week-2-js/easy/anagram.js
@@ -0,0 +1,15 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function sortString(str) {
+  return str.toLowerCase().split("").sort().join("");
+}
+
+function isAnagram(str1, str2) {
+  return sortString(str1) === sortString(str2);
+}
+
+module.exports = isAnagram;
---
+++ 02-1-week-2-js/easy/expenditure-analysis.js
@@ -0,0 +1,82 @@
+/*
+  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+  and return a list of objects where each object is unique category-wise and has total price spent as its value.
+  transactions is an array where each
+  Transaction - an object like 
+        {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	}
+  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+*/
+
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+];
+// [
+//   { category: "Food", totalSpent: 30 },
+//   { category: "Clothing", totalSpent: 40 },
+//   { category: "Electronics", totalSpent: 30 },
+// ];
+
+function calculateTotalSpentByCategory(transactions) {
+  let categoryStore = {};
+
+  for (let i = 0; i < transactions.length; i++) {
+    const { category, price } = transactions[i];
+
+    if (categoryStore[category]) {
+      categoryStore[category] = categoryStore[category] + price;
+    } else {
+      categoryStore[category] = price;
+    }
+  }
+
+  let finalArray = Object.keys(categoryStore).map((item) => {
+    return {
+      category: item,
+      totalSpent: categoryStore[item],
+    };
+  });
+
+  return finalArray;
+}
+
+module.exports = calculateTotalSpentByCategory;
